fb70a334985af295d8c45aba1b05cf075f1098fd
Trishna <trishna.patil13@gmail.com>
2024-08-12 12:12:16 -0400

    adding styles and paddings to questions list page

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dart_quiz/answer_button.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -12,6 +13,38 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text("Questions Screen");
+    return SizedBox(
+      width: double.infinity, //be as wide as possible
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The question..',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(
+            answerText: 'Answer Text',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer Text',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer Text',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer Text',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
